ee68ef31b68d498f332598c78bd2b51887f260eb
rfajri27 <rfajri255@gmail.com>
2022-09-03 20:42:00 +0700

    update & run diagnostics.py

--- diagnostics.py
+++ diagnostics.py
@@ -4,38 +4,92 @@ import numpy as np
 import timeit
 import os
 import json
+from joblib import load
+from scipy.sparse import data
+from training import preprocess_data
+import subprocess
+import sys
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+model_path = os.path.join(config['prod_deployment_path'])
 test_data_path = os.path.join(config['test_data_path']) 
 
 ##################Function to get model predictions
-def model_predictions():
+def model_predictions(dataset_path):
     #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+    model = load(os.path.join(model_path, "trainedmodel.pkl"))
+    encoder = load(os.path.join(model_path, "encoder.pkl"))
+    
+    if dataset_path is None: dataset_path = "testdata.csv"
+    df = pd.read_csv(os.path.join(test_data_path, dataset_path))
+
+    df_x, df_y, _ = preprocess_data(df, encoder)
+
+    y_pred = model.predict(df_x)
+
+    return y_pred, df_y
 
 ##################Function to get summary statistics
 def dataframe_summary():
     #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+    df = pd.read_csv(os.path.join(test_data_path, "testdata.csv"))
+    numeric_columns = [
+        "lastmonth_activity",
+        "lastyear_activity",
+        "number_of_employees"
+        ]
+    
+    result = []
+    for column in numeric_columns:
+        result.append([column, "mean", df[column].mean()])
+        result.append([column, "median", df[column].median()])
+        result.append([column, "standard deviation", df[column].std()])
+    
+    return result
+
+def missing_data():
+    #calculate summary statistics here
+    df = pd.read_csv(os.path.join(test_data_path, "testdata.csv"))
+    
+    result = []
+    for column in df.columns:
+        count_na = df[column].isna().sum()
+        count_not_na = df[column].count()
+        count_total = count_not_na + count_na
+
+        result.append([column, str(int(count_na/count_total*100))+"%"])
+    
+    return str(result)
 
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    result = []
+    for procedure in ["training.py" , "ingestion.py"]:
+        starttime = timeit.default_timer()
+        os.system('python3 %s' % procedure)
+        timing=timeit.default_timer() - starttime
+        result.append([procedure, timing])
+ 
+    return str(result) #return a list of 2 timing values in seconds
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    #get a list of
+    outdated_packages = subprocess.check_output(['pip', 'list', '--outdated']).decode(sys.stdout.encoding)
+    
+    return str(outdated_packages)
+ 
 
 
 if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
+    model_predictions(None)
     execution_time()
+    dataframe_summary()
+    missing_data()
     outdated_packages_list()
 
 
