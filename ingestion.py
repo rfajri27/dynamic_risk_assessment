cf3e1fc9e003da807f821df028797d63d5b0ad1f
rfajri27 <rfajri255@gmail.com>
2022-09-03 20:07:06 +0700

    update dan run ingestion.py

--- ingestion.py
+++ ingestion.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import glob
 import os
 import json
 from datetime import datetime
@@ -19,6 +20,17 @@ output_folder_path = config['output_folder_path']
 #############Function for data ingestion
 def merge_multiple_dataframe():
     #check for datasets, compile them together, and write to an output file
+    csv_files = glob.glob("%s/*.csv" % input_folder_path)
+
+    df = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
+
+    df.drop_duplicates(inplace=True)
+
+    df.to_csv("%s/finaldata.csv" % output_folder_path, index=False)
+
+    with open(os.path.join(output_folder_path, "ingestedfiles.txt"), "w") as report_file:
+        for line in csv_files:
+            report_file.write(line + "\n")
 
 
 
