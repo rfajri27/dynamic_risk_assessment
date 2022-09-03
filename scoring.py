0d6fc673f6be2446228c09ad3490b2c9a2f8d454
rfajri27 <rfajri255@gmail.com>
2022-09-03 21:18:18 +0700

    update

--- scoring.py
+++ scoring.py
@@ -18,6 +18,8 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
 test_data_path = os.path.join(config['test_data_path'])
+model_path = os.path.join(config['output_model_path'])
+# model_path = os.path.join(config['prod_deployment_path'])
 output_folder_path = os.path.join(config['output_folder_path']) 
 
 
@@ -26,10 +28,8 @@ def score_model(production=False):
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
     if production :
-        model_path = os.path.join(config['prod_deployment_path'])
         df = pd.read_csv(os.path.join(output_folder_path, "finaldata.csv"))
     else:
-        model_path = os.path.join(config['output_model_path'])
         df = pd.read_csv(os.path.join(test_data_path, "testdata.csv"))
     
     model = load(os.path.join(model_path, "trainedmodel.pkl"))
